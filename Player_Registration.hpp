8b26f7ef965be0178fe6b9fbc1d1f4d1ac88ed9f
ReihanPramudito <reihan.dp2005@gmail.com>
2025-06-01 21:42:00 +0800

    priority and withdrawal COMPLETE

--- Player_Registration.hpp
+++ Player_Registration.hpp
@@ -2,6 +2,7 @@
 #define PLAYER_REGISTRATION_HPP
 
 #include <string>
+#include <iostream>
 using namespace std;
 
 struct playerNode
@@ -20,20 +21,187 @@ class PlayerRegistration
     playerNode* front;
     playerNode* rear;
     string queueName;
-    int size = 0;
+    int size;
 
-    void insertAfter(playerNode* pos, playerNode* node);
+    void insertAfter(playerNode* lastPosition, playerNode* newNode)
+    {
+        if (lastPosition == nullptr)
+        {
+            if (isEmpty())
+            {
+                front = rear = newNode;
+            }
+            else
+            {
+                newNode->next = front;
+                newNode->prev = nullptr;
+                front->prev = newNode;
+                front = newNode;
+            }
+        }
+        else
+        {
+            newNode->next = lastPosition->next;
+            newNode->prev = lastPosition;
+            if (lastPosition->next != nullptr)
+            {
+                lastPosition->next->prev = newNode;
+            }
+            lastPosition->next = newNode;
+            if (lastPosition == rear)
+            {
+                rear = newNode; 
+            }
+        }
+    }
+
 public:
-    PlayerRegistration(string queueName = "Registration Queue");
-    ~PlayerRegistration();
-    bool isEmpty();
-    int getSize();
-    void registerPlayer(int id, string name, bool isEarlyBird, bool isWildcard);
-    playerNode* popFront();
-    bool removeById(int playerId);
-    void displayQueue();
-    
+    PlayerRegistration(string queueName)
+    {
+        front = nullptr;
+        rear = nullptr;
+        this->queueName = queueName;
+        size = 0;
+    }
 
+    ~PlayerRegistration()
+    {
+        cout << queueName << " removed" << endl;
+    }
+
+    bool isEmpty()
+    {
+        if (front == nullptr)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int getSize()
+    {
+        return size;
+    }
+
+    playerNode* registerPlayer(int id, string name, bool isEarlyBird, bool isWildcard)
+    {
+        playerNode* newNode = new playerNode;
+        newNode->id = id;
+        newNode->name = name;
+        newNode->isEarlyBird = isEarlyBird;
+        newNode->isWildcard = isWildcard;
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+
+
+        if (isEmpty())
+        {
+            front = rear = newNode;
+            size = 1;
+        }
+        else if (isWildcard)
+        {
+            playerNode* current = front;
+            playerNode* last = nullptr;
+            while (current && current->isWildcard)
+            {
+                last = current;
+                current = current->next;
+            }
+
+            insertAfter(last, newNode);
+            size++;
+        }
+        else if (isEarlyBird)
+        {
+            playerNode* current = front;
+            playerNode* last = nullptr;
+            while (current && (current->isWildcard || current->isEarlyBird))
+            {
+                last = current;
+                current = current->next;
+            }
+
+            insertAfter(last, newNode);
+            size++;
+        }
+        else
+        {
+            newNode->prev = rear;
+            newNode->next = nullptr;
+            rear->next = newNode;
+            rear = newNode;
+            size++;
+        }
+    }
+
+    playerNode* popFront()  // remove player from the front of the queue
+    {
+        if (isEmpty())
+        {
+            cout << "Registration Queue is empty." << endl;
+            return nullptr; // Queue is empty
+        }
+        playerNode* node = front;
+        front = front->next;
+        if (front != nullptr)
+        {
+            front->prev = nullptr;
+        }
+        else
+        {
+            rear = nullptr; // Queue is now empty
+        }
+        size--;
+        return node; // Return the removed player node
+    }
+
+    bool removeById(int playerId)
+    {
+        playerNode* current = front;
+        while (current != nullptr)
+        {
+            if (current->id == playerId)
+            {
+                if (current->prev != nullptr)
+                {
+                    current->prev->next = current->next;
+                }
+                else{
+                    front = current->next; 
+                }
+                
+                if (current->next != nullptr)
+                {
+                    current->next->prev = current->prev;
+                }
+                else
+                {
+                    rear = current->prev; 
+                }
+
+                cout << "Player: " << current->name << " with ID " << playerId << " was removed from the queue." << endl;
+                delete current;
+                size--;
+                return true;
+            }
+            current = current->next;
+        }
+        cout << "Player with ID " << playerId << " not found in the queue." << endl;
+        return false; 
+    }
+
+    void displayQueue()
+    {
+        cout << queueName << " with " << size << " players registered: " << endl;
+        playerNode* current = front;
+        while (current != nullptr)
+        {
+            cout << "[" << current->id << ": " << current->name << "]" << endl;
+            current = current->next;
+        }
+    }
+    
 };
 
 #endif
